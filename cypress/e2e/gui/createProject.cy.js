c821de46f2f29a23d8d9e4e82e45d19a5e8f59a0
Lion Simon <lionsimongondim@gmail.com>
2023-04-18 18:25:56 -0300

    Aula 4 - como manter a sessão com cy.session

---
+++ cypress/e2e/gui/createProject.cy.js
@@ -0,0 +1,24 @@
+import { faker } from "@faker-js/faker";
+
+describe("Create Projetct", () => {
+  beforeEach(() => {
+    cy.login();
+  });
+
+  it("sucessfully", () => {
+    const project = {
+      //essa nomemclatura do "name" é a forma de concatenar um comando JS (faker... dentro do ${}) com uma string (project-). Isso se chama "Template Literals".
+      name: `project-${faker.datatype.uuid()}`,
+      description: faker.random.words(5),
+    };
+
+    cy.gui_createProject(project);
+
+    cy.url().should(
+      "be.equal",
+      `${Cypress.config("baseUrl")}/${Cypress.env("user_name")}/${project.name}`
+    );
+    cy.contains(project.name).should("be.visible");
+    cy.contains(project.description).should("be.visible");
+  });
+});
